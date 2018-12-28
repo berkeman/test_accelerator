f8aa7345c4dbcf21cce7cf0c808e27644be60647
Anders Berkeman <anders@berkeman.org>
2018-12-28 15:40:28 +0000

    [test] tests for dummy source & target + ignored workdirs

--- test.py
+++ test.py
@@ -117,4 +117,20 @@ test.configure(defined_workdirs=wds, sources=wds)
 parsed, output = test.run()
 assert not parsed['serving'] and '# Error in configfile, must specify target_workdir.' in output[0]
 
+# define 0...7; target 0; source 0...3; => create 0...3; twist: only 0...3 should be created and 1...3 are sources, 4...7 should be ignored
+wds = test.new(num_workdirs=8)
+test.configure(defined_workdirs=wds, target=wds[0], sources=wds[:4])
+parsed, output = test.run()
+assert parsed['serving'] and parsed['created'] == set(wds[:4]) and parsed['target'] == wds[0] and parsed['sources'] == set(wds[1:4])
 
+# define 0, 1; target=dummy; twist: target does not exist
+wds = test.new(num_workdirs=2)
+test.configure(defined_workdirs=wds, target='dummy')
+parsed, output = test.run()
+assert not parsed['serving'] and 'ERROR:  Workdir(s) missing definition: "dummy".' in output[0]
+
+# define 0, 1; target=0; sources=dummy; twist: source does not exist
+wds = test.new(num_workdirs=2)
+test.configure(defined_workdirs=wds, target=wds[0], sources=['dummy',])
+parsed, output = test.run()
+assert not parsed['serving'] and 'ERROR:  Workdir(s) missing definition: "dummy".' in output[0]
